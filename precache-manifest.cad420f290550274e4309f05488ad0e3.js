ee56c742fe64b69e4e0212ce3df6386959bede61
ywan168 <54960821+ywan168@users.noreply.github.com>
2023-03-16 14:52:10 -0500

    Updates

---
+++ precache-manifest.cad420f290550274e4309f05488ad0e3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4ceb14d2823870850146",
+    "url": "/MovieDB-App/static/css/main.8d6b2795.chunk.css"
+  },
+  {
+    "revision": "4ceb14d2823870850146",
+    "url": "/MovieDB-App/static/js/main.785857fa.chunk.js"
+  },
+  {
+    "revision": "efdfb694ad7aa3e99145",
+    "url": "/MovieDB-App/static/js/runtime~main.ab809075.js"
+  },
+  {
+    "revision": "7a8b0973a718923de2c4",
+    "url": "/MovieDB-App/static/js/2.9d9466d9.chunk.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/MovieDB-App/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "b65421cc1341422e51c064d71d3f9aab",
+    "url": "/MovieDB-App/index.html"
+  }
+];
