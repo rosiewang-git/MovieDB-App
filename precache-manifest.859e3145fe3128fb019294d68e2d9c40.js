ed532f311f0e7333fe0a3d2a1e99c41c005fc93a
ywan168 <54960821+ywan168@users.noreply.github.com>
2023-03-16 23:10:48 -0500

    Updates

--- precache-manifest.859e3145fe3128fb019294d68e2d9c40.js
+++ precache-manifest.859e3145fe3128fb019294d68e2d9c40.js
@@ -1,26 +1,26 @@
 self.__precacheManifest = [
   {
-    "revision": "685e995fa9068dbf1a84",
+    "revision": "9dde543558ed4c707775",
     "url": "/MovieDB-App/static/css/main.8d6b2795.chunk.css"
   },
   {
-    "revision": "685e995fa9068dbf1a84",
-    "url": "/MovieDB-App/static/js/main.cd8e1484.chunk.js"
+    "revision": "9dde543558ed4c707775",
+    "url": "/MovieDB-App/static/js/main.8e7c8e0c.chunk.js"
   },
   {
     "revision": "efdfb694ad7aa3e99145",
     "url": "/MovieDB-App/static/js/runtime~main.ab809075.js"
   },
   {
-    "revision": "0a8a85decc880023f5f1",
-    "url": "/MovieDB-App/static/js/2.fb9f8ebe.chunk.js"
+    "revision": "74b0b8c8bd8e8ec9a20f",
+    "url": "/MovieDB-App/static/js/2.02a4094c.chunk.js"
   },
   {
     "revision": "06e733283fa43d1dd57738cfc409adbd",
     "url": "/MovieDB-App/static/media/logo.06e73328.svg"
   },
   {
-    "revision": "b7c83df9d3402f5a433c96f4221c3a70",
+    "revision": "f78c85ed44957606db1436a307a912d7",
     "url": "/MovieDB-App/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/MovieDB-App/precache-manifest.b3261d8c317bd4eb07ee407d54be1e1c.js"
+  "/MovieDB-App/precache-manifest.859e3145fe3128fb019294d68e2d9c40.js"
 );
 
 workbox.clientsClaim();
